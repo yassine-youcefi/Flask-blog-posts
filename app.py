c28c09b0d75ae821529ae0fac7f630dd8afd8baa
yani27 <yanilacamora@gmail.com>
2021-04-23 15:17:20 +0100

    setup & start wis bootsrap styling

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 application = Flask(__name__)
 
+posts = [
+    {
+        'author': 'yassine youcefi',
+        'title': 'Blog jdid hada 1',
+        'content': 'my first blog for test it is hardcoded',
+        'date_posted': 'April 21'
+    },
+    {
+        'author': 'chaindraa wahid',
+        'title': 'Blog jdid hada 2',
+        'content': 'my first blog for test it is hardcoded',
+        'date_posted': 'April 22'
+
+    }
+]
+
 
 @application.route('/')
-def index(name):
-    return "hellow there"
+@application.route('/home')
+def home():
+    return render_template('index.html', posts=posts)
 
+
+@application.route('/about')
+def about():
+    return render_template('about.html', title='About')
 
 # if __name__ == "__main__":
 #     application.run(debug=True, port=5050)
