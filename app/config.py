ebec1491468aa7f6ed15871852fe4a060d0dea08
yani27 <yanilacamora@gmail.com>
2021-05-01 18:27:55 +0100

    add secretkey generetor

--- app/config.py
+++ app/config.py
@@ -1,10 +1,11 @@
 import os
+import secrets
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    #DEBUG = True
-    SECRET_KEY = 'bd11ed9625dba9f37dccc7d967b056f5'
+    DEBUG = True
+    SECRET_KEY = secrets.token_urlsafe(16)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'Database/app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
